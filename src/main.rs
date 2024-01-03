eb909ef317323e9ba85e265b68270a1caadee291
KazumaTakata <k.takata@qufooit.com>
2024-01-03 15:37:14 +0900

    refactor neuralnet model

--- src/main.rs
+++ src/main.rs
@@ -2,54 +2,14 @@ mod board;
 mod csa;
 mod piece_type;
 mod position;
-
-use candle_core::{DType, Device, Result, Tensor, D};
-use candle_nn::{loss, ops, Conv2d, Linear, Module, ModuleT, Optimizer, VarBuilder, VarMap};
-
-#[derive(Debug)]
-struct ConvNet {
-    conv1: Conv2d,
-    conv2: Conv2d,
-    fc1: Linear,
-    fc2: Linear,
-    dropout: candle_nn::Dropout,
-}
-
-impl ConvNet {
-    fn new(vs: VarBuilder) -> Result<Self> {
-        let conv1 = candle_nn::conv2d(1, 32, 5, Default::default(), vs.pp("c1"))?;
-        let conv2 = candle_nn::conv2d(32, 64, 5, Default::default(), vs.pp("c2"))?;
-        let fc1 = candle_nn::linear(1024, 1024, vs.pp("fc1"))?;
-        let fc2 = candle_nn::linear(1024, 10, vs.pp("fc2"))?;
-        let dropout = candle_nn::Dropout::new(0.5);
-        Ok(Self {
-            conv1,
-            conv2,
-            fc1,
-            fc2,
-            dropout,
-        })
-    }
-
-    fn forward(&self, xs: &Tensor, train: bool) -> Result<Tensor> {
-        let xs = xs
-            .apply(&self.conv1)?
-            .max_pool2d(2)?
-            .apply(&self.conv2)?
-            .max_pool2d(2)?
-            .flatten_from(1)?
-            .apply(&self.fc1)?
-            .relu()?;
-        self.dropout.forward_t(&xs, train)?.apply(&self.fc2)
-    }
-}
+mod neural;
 
 fn main() {
     let csa_file = csa::parse_csa_file();
 
     let mut board = board::initialize_board();
 
-    println!("tensor shape: {:?}", board.to_tensor().shape().dims3()); 
+    println!("tensor shape: {:?}", board.to_tensor().shape().dims3());
 
     // for next_move in csa_file.moves.iter() {
     //     board.pprint();
@@ -58,7 +18,6 @@ fn main() {
     // }
 
     // let input_tensor = Tensor::rand(-1.0f32, 1.0, (1, 1, 28, 28), &Device::Cpu).unwrap();
-
 
     // let mut varmap = VarMap::new();
     // let vs = VarBuilder::from_varmap(&varmap, DType::F32, &Device::Cpu);
---
+++ src/neural.rs
@@ -0,0 +1,40 @@
+use candle_core::{DType, Device, Result, Tensor, D};
+use candle_nn::{loss, ops, Conv2d, Linear, Module, ModuleT, Optimizer, VarBuilder, VarMap};
+
+#[derive(Debug)]
+struct ConvNet {
+    conv1: Conv2d,
+    conv2: Conv2d,
+    fc1: Linear,
+    fc2: Linear,
+    dropout: candle_nn::Dropout,
+}
+
+impl ConvNet {
+    fn new(vs: VarBuilder) -> Result<Self> {
+        let conv1 = candle_nn::conv2d(1, 32, 5, Default::default(), vs.pp("c1"))?;
+        let conv2 = candle_nn::conv2d(32, 64, 5, Default::default(), vs.pp("c2"))?;
+        let fc1 = candle_nn::linear(1024, 1024, vs.pp("fc1"))?;
+        let fc2 = candle_nn::linear(1024, 10, vs.pp("fc2"))?;
+        let dropout = candle_nn::Dropout::new(0.5);
+        Ok(Self {
+            conv1,
+            conv2,
+            fc1,
+            fc2,
+            dropout,
+        })
+    }
+
+    fn forward(&self, xs: &Tensor, train: bool) -> Result<Tensor> {
+        let xs = xs
+            .apply(&self.conv1)?
+            .max_pool2d(2)?
+            .apply(&self.conv2)?
+            .max_pool2d(2)?
+            .flatten_from(1)?
+            .apply(&self.fc1)?
+            .relu()?;
+        self.dropout.forward_t(&xs, train)?.apply(&self.fc2)
+    }
+}
