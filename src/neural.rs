f58c5447acf02f1e98e3520998ffc7f2861388e7
KazumaTakata <k.takata@qufooit.com>
2024-01-06 15:28:31 +0900

    rm neuralnet

--- src/neural.rs
+++ src/neural.rs
@@ -1,7 +1,7 @@
 use candle_core::{DType, Device, Result, Tensor, D};
 use candle_nn::{batch_norm, Conv2dConfig};
 use candle_nn::{loss, ops, Conv2d, Linear, Module, ModuleT, Optimizer, VarBuilder, VarMap};
-
+#[derive(Debug)]
 pub struct Resnet {
     conv1: Conv2d,
     batch_norm1: batch_norm::BatchNorm,
@@ -74,7 +74,7 @@ impl Resnet {
         return Ok(ys);
     }
 }
-
+#[derive(Debug)]
 struct ResnetBlock {
     conv1: Conv2d,
     bn1: batch_norm::BatchNorm,
@@ -128,43 +128,5 @@ impl ResnetBlock {
             .apply_t(&self.bn2, train)?;
         let ys = (ys + xs)?.relu()?;
         return Ok(ys);
-    }
-}
-
-#[derive(Debug)]
-struct ConvNet {
-    conv1: Conv2d,
-    conv2: Conv2d,
-    fc1: Linear,
-    fc2: Linear,
-    dropout: candle_nn::Dropout,
-}
-
-impl ConvNet {
-    fn new(vs: VarBuilder) -> Result<Self> {
-        let conv1 = candle_nn::conv2d(1, 32, 5, Default::default(), vs.pp("c1"))?;
-        let conv2 = candle_nn::conv2d(32, 64, 5, Default::default(), vs.pp("c2"))?;
-        let fc1 = candle_nn::linear(1024, 1024, vs.pp("fc1"))?;
-        let fc2 = candle_nn::linear(1024, 10, vs.pp("fc2"))?;
-        let dropout = candle_nn::Dropout::new(0.5);
-        Ok(Self {
-            conv1,
-            conv2,
-            fc1,
-            fc2,
-            dropout,
-        })
-    }
-
-    fn forward(&self, xs: &Tensor, train: bool) -> Result<Tensor> {
-        let xs = xs
-            .apply(&self.conv1)?
-            .max_pool2d(2)?
-            .apply(&self.conv2)?
-            .max_pool2d(2)?
-            .flatten_from(1)?
-            .apply(&self.fc1)?
-            .relu()?;
-        self.dropout.forward_t(&xs, train)?.apply(&self.fc2)
     }
 }
